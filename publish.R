ab0af8fd1de0c21519b66b04ac6c5c3f1aac5425
filipwastberg <filip.wastberg@gmail.com>
2019-08-28 16:38:20 +0200

    lagt upp materialet

---
+++ publish.R
@@ -0,0 +1,8 @@
+rsconnect::setAccountInfo(name='filipwastberg',
+																										token='AE35ED936056D1F756D7F5D35B35B3BF',
+																										secret='qggpPiyGj5/7cWKPemvwASU11kJ5lCv7+0NMlxYs')
+
+bookdown::publish_book(name = "tele2-intro-to-r", account = "filip.wastberg")
+
+bookdown::publish_book(name = "intro-to-r-ferro", account = "filip.wastberg")
+
